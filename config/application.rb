f6b95b20bc15d220936bd34f33c6a41c32252d8b
takuru <asaumi@kray.jp>
2018-11-01 15:28:17 +0900

    rails初期設定

--- config/application.rb
+++ config/application.rb
@@ -22,12 +22,15 @@ module RailsChat
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 5.2
 
-    # Settings in config/environments/* take precedence over those specified here.
-    # Application configuration can go into files in config/initializers
-    # -- all .rb files in that directory are automatically loaded after loading
-    # the framework and any gems in your application.
+    config.paths.add 'lib', eager_load: true
+    config.time_zone = 'Tokyo'
+    config.i18n.default_locale = :ja
+    config.i18n.load_path += Dir[Rails.root.join('config', 'locales', '**', '*.{rb,yml}').to_s]
 
-    # Don't generate system test files.
-    config.generators.system_tests = nil
+    config.generators do |g|
+      g.assets false
+      g.helper false
+      g.system_tests false
+    end
   end
 end
