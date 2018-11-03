d7e40c5359a85ab362505e63ec6ec1c0c5f052de
takuru <asaumi@kray.jp>
2018-11-03 10:28:34 +0900

    Rooms#showのテスト作成

--- spec/controllers/rooms_controller_spec.rb
+++ spec/controllers/rooms_controller_spec.rb
@@ -5,7 +5,38 @@ RSpec.describe RoomsController do
     before { get :index }
 
     it '200 OKが返る' do
-      expect(response).to be_success
+      expect(response).to have_http_status 200
+    end
+  end
+
+  describe 'GET /rooms/:id' do
+    subject { get :show, params: {id: room_name} }
+
+    context '指定のルームが存在しない' do
+      let(:room_name) { 'my room' }
+
+      it '200 OKが返る' do
+        subject
+        expect(response).to have_http_status 200
+      end
+
+      it '新規ルームが作成される' do
+        expect { subject }.to change { Room.count }.by(1)
+      end
+    end
+
+    context '指定のルームが存在する' do
+      let!(:room) { create :room }
+      let(:room_name) { room.name }
+
+      it '200 OKが返る' do
+        subject
+        expect(response).to have_http_status 200
+      end
+
+      it '新規ルームが作成されない' do
+        expect { subject }.to_not change { Room.count }
+      end
     end
   end
 end
