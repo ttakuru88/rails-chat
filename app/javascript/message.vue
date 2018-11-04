ac435571474899eff4dbc48e73ea19780ca051ac
takuru <asaumi@kray.jp>
2018-11-04 10:14:40 +0900

    日付はjs側で整形

--- app/javascript/message.vue
+++ app/javascript/message.vue
@@ -1,10 +1,12 @@
 <template>
   <li class="list-group-item">
-    <strong>{{ userName }}</strong>：{{ message.text }} <small>({{ message.created_at }})</small>
+    <strong>{{ userName }}</strong>：{{ message.text }} <small>({{ createdAt }})</small>
   </li>
 </template>
 
 <script>
+import * as moment from 'moment'
+
 export default {
   props: ['message'],
   data: function () {
@@ -18,6 +20,19 @@ export default {
   computed: {
     userName: function() {
       return this.message.user_name || '名無し'
+    },
+    createdAt: function() {
+      const now = moment()
+      const createdAt = moment(this.message.created_at)
+      if(now.year() != createdAt.year()){
+        return createdAt.format('YYYY-MM-DD HH:mm')
+      }
+      else if(now.month() != createdAt.month() && now.date() != createdAt.date()){
+        return createdAt.format('MM-DD HH:mm')
+      }
+      else {
+        return createdAt.format('HH:mm')
+      }
     }
   }
 }
