09d889faa09995bb36abb85ef4bc2c56b50d18c0
takuru <asaumi@kray.jp>
2018-11-04 14:26:38 +0900

    名前とメッセージに入力文字数制限

--- app/javascript/speaker.vue
+++ app/javascript/speaker.vue
@@ -2,10 +2,10 @@
   <form @submit.prevent="speak">
     <div class="form-row mb-4" >
       <div class="col-2">
-        <input v-model="userName" placeholder="名前" class="form-control">
+        <input v-model="userName" placeholder="名前" class="form-control" maxlength="32">
       </div>
       <div class="col">
-        <input v-model="message" placeholder="発言する" class="form-control">
+        <input v-model="message" placeholder="発言する" class="form-control" maxlength="172">
       </div>
       <div class="col"
         <input class="btn btn-primary" type="submit" value="発言">
